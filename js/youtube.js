c71f3569894f513e109e45ecef063c24115aecb7
jingeon27 <jingeon27@gmail.com>
2021-07-08 09:53:54 +0900

    start project

---
+++ js/youtube.js
@@ -0,0 +1,32 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8',
+    playerVars: {
+        autoplay: true,
+        loop: true,
+        playlist: 'An6LvWQuj_8'
+    },
+    events: {
+        onReady: function (event) {
+            event.target.mute()
+        }
+    }
+  });
+}
+
+function floatingObject(selector) {
+    gsap.to(selector, 1, {
+        y: 20,
+        repeat: -1,
+        yoyo: true
+    });
+}
+floatingObject('.floating');
